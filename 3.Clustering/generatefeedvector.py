0b6f3d37cdd8a5199666165835bf5cfef4013c6a
Kimmirikwa <kimrodrikwa@gmail.com>
2018-03-25 00:00:07 +0300

    add the file to generate the feed data

--- 3.Clustering/generatefeedvector.py
+++ 3.Clustering/generatefeedvector.py
@@ -15,7 +15,7 @@ def getwordcounts(url):
 		else:
 			summary = e.description
 
-		words = getWords(e.title + summary)
+		words = getwords(e.title + summary)
 		for word in words:
 			wc.setdefault(word, 0)
 			wc[word] += 1
@@ -31,3 +31,39 @@ def getwords(html):
 	words = re.compile(r'[^A-Z^a-z]+').split(txt)
 
 	return [word.lower() for word in words if word != ' ']
+
+
+apcount = {}
+wordcount = {}
+
+path = '/home/mirikwa/projects/ml/Programming-Collective-Intelligence/3.Clustering/data'
+
+for feedurl in file(path + '/feedlist.txt'):
+	title, wc = getwordcounts(feedurl)
+	wordcount[title] = wc
+	for word, count in wc.items():
+		apcount.setdefault(word, 0)
+		if count <= 1:
+			continue
+		apcount[word] += 1
+
+
+# gettingn the wors that will actually be used in clustering
+# dropping words that are too common or very strange and thus appear in very few blogs
+wordlist = []
+feedlist = open(path + '/feedlist.txt').readlines()
+for word, count in apcount.items():
+	frac = float(count) / len(feedlist)
+	if frac > 0.1 and frac < 0.5:
+		wordlist.append(word)
+
+out = file(path + '/blogdata.txt', 'w')
+out.write('Blog')
+for word in wordlist:
+	out.write('\t{}'.format(word))
+for word, count in wordcount.items():
+	if word in wordlist:
+		out.write('\t{}'.format(count))
+	else:
+		out.write('\t0')
+	out.write('\n')
