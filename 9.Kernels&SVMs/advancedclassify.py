c0e6794bfdd1fa33ec015ad288e8b81a8a646293
Kimmirikwa <kimrodrikwa@gmail.com>
2018-03-28 14:00:40 +0300

    training the svm model

--- 9.Kernels&SVMs/advancedclassify.py
+++ 9.Kernels&SVMs/advancedclassify.py
@@ -2,6 +2,7 @@ from pylab import plot, show
 from geopy.geocoders import Nominatim
 import math
 from math import sqrt
+from svmutil import *
 
 location_cache = {}  # this will cache the coordinates of the locations, to prevent fetching the location again
 
@@ -14,6 +15,12 @@ class matchrow:
         last value will be used in the classification
         the last value indicates whether or not someone is considered a good match
         allnum - shows if all the columns of the row are numbers
+
+        for example:
+            1. a row froms ages only looks like this:
+                24,30,1
+                24 and 30 are the ages and 1 indicates that the two people matched
+            A row from matchmaker.csv has more fields and not all of them are numerical
         '''
         self.data = [float(row[i]) for i in range(len(row) - 1)] if allnum\
             else row[0:len(row) - 1]
@@ -39,13 +46,13 @@ def plotagematches(rows):
     The matching will only be based on age, which is quite simple
     '''
     xdm, ydm = [r.data[0] for r in rows if r.match == 1], \
-        [r.data[1] for r in rows if r.match == 1]
+        [r.data[1] for r in rows if r.match == 1]  # the matched ages
 
     xdn, ydn = [r.data[0] for r in rows if r.match == 0], \
-        [r.data[1] for r in rows if r.match == 0]
+        [r.data[1] for r in rows if r.match == 0]  # the ages that did not match
 
-    plot(xdm, ydm, 'go')
-    plot(xdn, ydn, 'rx')
+    plot(xdm, ydm, 'go')  # matched ages will be plotted with green 'os'
+    plot(xdn, ydn, 'rx')  #unmatched ages will be plotted with red 'xs'
 
     show()
 
@@ -119,6 +126,7 @@ def yesno(v):
 def matchcount(interest1, interest2):
     '''
     returns the numbers of common interests between 2 people
+    this technique however has a potential of losing some important information
     '''
     interest1 = interest1.split(':')  # the interests are split by ':'
     interest2 = interest2.split(':')
@@ -136,7 +144,7 @@ def getlocation(address):
     '''
     if address in location_cache:
         return location_cache[address]
-    geolocator = Nominatim()
+    geolocator = Nominatim(timeout=5)
     location = geolocator.geocode(address)
     lat = location.latitude  # the latitude
     longi = location.longitude  # the longitude
@@ -161,8 +169,8 @@ def loadnumerical():
     creates a new dataset from the dataset in matchmaker.csv
     the non-numerical data points are converted to numerical ones
     '''
-    oldrows = loadmatch('matchmaker.csv')  # consists of numerical and non-numerical data
-    newrows = []  # will hold the new data having only numerical data
+    oldrows = loadmatch('/home/mirikwa/projects/ml/Programming-Collective-Intelligence/9.Kernels&SVMs/data/matchmaker.csv')  # consists of numerical and non-numerical data
+    newrows = []  # will hold the new data having only numerical data, after transformatio using the methods above on categorical data
     count = 0
     for row in oldrows:
         count += 1
@@ -179,6 +187,7 @@ def scaledata(rows):
     scales data to be in the same range
     if a specific date point is in a bigger scale than the rest, its contribution to learning will be greater than
     other data points. The data ranges after scaling will be between 0 and 1
+    scaling the data makes differences between the variables to be comparable
     '''
     low = [999999999.0] * len(rows[0].data)  # will hold the minimum of every data column
     high = [-999999999.0] * len(rows[0].data)  # will hold the maximum of every data column
@@ -214,6 +223,12 @@ def rbf(v1, v2, gamma=20):
 
 
 def nlclassify(point, rows, offset, gamma):
+    '''
+    classifies the point
+    the average of each class in not calculated here since
+    dot product of an average of vectors and vector A is equal to the the average of the
+    dot products of the vectors and vector A
+    '''
     sum0 = 0.0
     sum1 = 0.0
     count0 = 0.0
@@ -235,6 +250,10 @@ def nlclassify(point, rows, offset, gamma):
 
 
 def getoffset(rows, gamma=10):
+    '''
+    The offset value should be calculated only once for a dataset as 
+    it is computationally expensive
+    '''
     l0 = []
     l1 = []
 
@@ -248,3 +267,22 @@ def getoffset(rows, gamma=10):
     sum1 = sum(sum([rbf(v1, v2, gamma) for v1 in l1]) for v2 in l1)
 
     return (1.0/(len(l1)**2))*sum1-(1.0/(len(l0)**2))*sum0
+
+
+def svmtrain():
+    '''
+    trains an svm model and returns it
+    '''
+    # preparing the data to be used on the model
+    numericalset = loadnumerical()
+    scaledset,scalef = scaledata(numericalset)
+    # the model expects a tuple of the lists of inputs and outputs
+    answers,inputs = [r.match for r in scaledset],[r.data for r in scaledset]
+
+    # setting up the SVM model
+    prob = svm_problem(answers,inputs)
+    param = svm_parameter()
+    param.kernel_type = RBF
+    param.C = 10
+
+    return svm_train(prob, param)
